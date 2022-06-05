b4537879a00acf615a5715b8d5e5240bccf4ac76
necatiozmen <necatiozmn@gmail.com>
2022-06-05 14:36:41 +0300

    Add api side

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken");
@@ -21,10 +22,39 @@ const users = [
 let refreshTokens = [];
 
 const generateAccessToken = (user) => {
-    jwt.sign({ id: user.id, isAdmin: user.isAdmin }, "mySecretKey", {
-        expiresIn: "5s"
+    return jwt.sign({ id: user.id, isAdmin: user.isAdmin }, "mySecretKey", {
+        expiresIn: "100s"
     })
 }
+const generateRefreshToken = (user) => {
+    return jwt.sign({ id: user.id, isAdmin: user.isAdmin }, "myRefreshSecretKey");
+};
+
+app.post("/api/refresh", (req, res) => {
+    //take the refresh token from the user
+    const refreshToken = req.body.token;
+
+    //send error if there is no token or it's invalid
+    if (!refreshToken) return res.status(401).json("You are not authenticated!");
+    if (!refreshTokens.includes(refreshToken)) {
+        return res.status(403).json("Refresh token is not valid!");
+    }
+
+    // remove used refresh token and create a new one
+    refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
+
+    const newAccessToken = generateAccessToken(user);
+    const newRefreshToken = generateRefreshToken(user);
+
+    refreshTokens.push(newRefreshToken);
+
+    //maybe send them in cookie
+    res.status(200).json({
+        accessToken: newAccessToken,
+        refreshToken: newRefreshToken,
+    });
+
+})
 
 app.post("/api/login", (req, res) => {
     const { username, password } = req.body;
@@ -32,20 +62,50 @@ app.post("/api/login", (req, res) => {
         return u.username === username && u.password === password;
     });
     if (user) {
-        //Generate an access token
         const accessToken = generateAccessToken(user);
-        /*  const refreshToken = generateRefreshToken(user); */
-        /*  refreshTokens.push(refreshToken); */
+        const refreshToken = generateRefreshToken(user);
+        refreshTokens.push(refreshToken);
         res.json({
             username: user.username,
             isAdmin: user.isAdmin,
             accessToken,
-            /*      refreshToken, */
+            refreshToken
         });
     } else {
         res.status(400).json("Username or password incorrect!");
     }
 })
 
+const verify = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (authHeader) {
+        const token = authHeader.split(" ")[1];
+
+        jwt.verify(token, "mySecretKey", (err, user) => {
+            if (err) {
+                return res.status(403).json("Token is not valid!");
+            }
+
+            req.user = user;
+            next();
+        });
+    } else {
+        res.status(401).json("You are not authenticated!");
+    }
+};
+
+app.delete("/api/users/:userId", verify, (req, res) => {
+    if (req.user.id === req.params.userId || req.user.isAdmin) {
+        res.status(200).json("User has been deleted.");
+    } else {
+        res.status(403).json("You are not allowed to delete this user!");
+    }
+});
+
+app.post("/api/logout", verify, (req, res) => {
+    const refreshToken = req.body.token;
+    refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
+    res.status(200).json("You logged out successfully.");
+});
 
 app.listen(5000, () => console.log("Backend server is running!"));
